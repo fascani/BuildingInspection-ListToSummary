15fe0332c929070852e55f5f600782d7a1abe092
Francois Ascani <francois.ascani@gmail.com>
2023-02-19 15:48:18 -1000

    debugging

--- BuildingInspection-ListToSummary.py
+++ BuildingInspection-ListToSummary.py
@@ -70,17 +70,17 @@ if check_password():
         st.session_state.comments = list()
 
     # Initialize new_comment
-    #if 'new_comment' not in st.session_state:
-    #    st.session_state.new_comment = None
+    if 'new_comment' not in st.session_state:
+        st.session_state.new_comment = ''
+
+    def new_comment():
+        st.session_state.count += 1
+        st.session_state.comments.append(st_session_state.new_comment)
 
     container = st.container()
     #container.text_input('Write your comment', on_change=new_comment, key='comment-' + str(st.session_state.count))
     container.text_input('Write your comment', on_change=new_comment, key='new_comment')
     
-    def new_comment():
-        st.session_state.count += 1
-        st.session_state.comments.append(st_session_state.new_comment)
-
     st.text(st.session_state.count)
     st.text(st.session_state.comments)
 
